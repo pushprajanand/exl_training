11c9100f9fa2a183cfe1e2cfc93d0081f6f2b3f9
pushpraj anand <47240459+pushprajanand@users.noreply.github.com>
2023-08-02 12:22:08 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace slambook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your fav movie name:");
+            string movieName = Console.ReadLine();
+            string output = $"You Entered {movieName}";
+            Console.WriteLine(output);
+            
+        }
+    }
+}
