9e45c21f39118385afef57087dfadfb3bcf7b10d
pushprajanand <pushprajanand10@gmail.com>
2023-08-02 13:00:15 +0530

    Changes to Program 3

---
+++ Program3.cs
@@ -0,0 +1,14 @@
+namespace slambook
+{
+    internal class Program3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your fav athelete name:");
+            string movieName = Console.ReadLine();
+            string output = $"You Entered {movieName}";
+            Console.WriteLine(output);
+            //this is a comment
+        }
+    }
+}
